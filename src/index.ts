cd18cd5e32e1f0744ad5231117bb7a37c4cc30ea
Francisco <ip4c0@icloud.com>
2022-04-25 20:30:54 -0500

    Interactions on click or keypress

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { dictionary } from './utils/dictionary';
 import { targetWords } from './utils/targetWords';
 
+const WORD_LENGTH = 5;
+
+const guessGrid = document.querySelector('[data-guess-grid]') as HTMLElement;
+
 startInteraction();
 
 function startInteraction() {
@@ -16,8 +20,8 @@ function stopInteraction() {
 function handleMouseClick(event: MouseEvent) {
   const target = event.target as HTMLElement;
 
-  if (target.matches('[data-key]')) {
-    // pressKey(target.dataset.key);
+  if (target.matches('[data-key]') && target.dataset.key) {
+    pressKey(target.dataset.key);
     return;
   }
 
@@ -44,7 +48,24 @@ function handleKeyPress(event: KeyboardEvent) {
   }
 
   if (event.key.match(/^[a-z]$/)) {
-    // pressKey(event.key);
+    pressKey(event.key);
     return;
   }
+}
+
+function pressKey(key: string) {
+  const activeTiles = getActiveFiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+
+  const nextTitle = guessGrid.querySelector(
+    ':not([data-letter])',
+  ) as HTMLElement;
+
+  nextTitle.dataset.letter = key.toLowerCase();
+  nextTitle.textContent = key;
+  nextTitle.dataset.state = 'active';
+}
+
+function getActiveFiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]');
 }
