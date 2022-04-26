29fa987de57f329c28b26181ff836973be60026d
Francisco <ip4c0@icloud.com>
2022-04-26 16:02:14 -0500

    Wordle Clone TS

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { dictionary } from './utils/dictionary';
 import { targetWords } from './utils/targetWords';
 
 const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
 
 const offsetFromData: any = new Date(2022, 0, 1);
 const msOffset = Date.now() - offsetFromData;
@@ -11,9 +13,10 @@ const targetWord = targetWords[dayOffset];
 const alertContainer = document.querySelector(
   '[data-alert-container]',
 ) as HTMLElement;
-
 const guessGrid = document.querySelector('[data-guess-grid]') as HTMLElement;
+const keyboard = document.querySelector('[data-keyboard]') as HTMLElement;
 
+console.log("Today's word is " + "'" + targetWord + "'");
 startInteraction();
 
 function startInteraction() {
@@ -89,12 +92,68 @@ function submitGuess() {
   if (acitveTiles.length !== WORD_LENGTH) {
     console.log('NOT LONG ENOUGH!');
     showAlert('Not enough letters!');
-    // shakeTiles(acitveTiles);
+    shakeTiles(acitveTiles);
     return;
   }
+
+  const guess = acitveTiles.reduce((word: string, tile: any) => {
+    return word + tile.dataset.letter;
+  }, '');
+
+  if (!dictionary.includes(guess)) {
+    showAlert('Not in dictionary!');
+    shakeTiles(acitveTiles);
+    return;
+  }
+
+  stopInteraction();
+  acitveTiles.forEach((...params) => flipTile(...params, guess));
 }
 
-function getActiveTiles() {
+function flipTile(
+  tile: HTMLElement,
+  index: number,
+  array: Element[],
+  guess: string,
+) {
+  const letter = tile.dataset.letter;
+  const key = keyboard.querySelector(`[data-key="${letter}"i]`);
+
+  setTimeout(() => {
+    tile.classList.add('flip');
+  }, (index * FLIP_ANIMATION_DURATION) / 2);
+
+  tile.addEventListener(
+    'transitionend',
+    () => {
+      tile.classList.remove('flip');
+      if (targetWord[index] === letter) {
+        tile.dataset.state = 'correct';
+        key?.classList.add('correct');
+      } else if (letter && targetWord.includes(letter)) {
+        tile.dataset.state = 'wrong-location';
+        key?.classList.add('wrong-location');
+      } else {
+        tile.dataset.state = 'wrong';
+        key?.classList.add('wrong');
+      }
+
+      if (index === array.length - 1) {
+        tile.addEventListener(
+          'transitionend',
+          () => {
+            startInteraction();
+            checkWinLose(guess, array);
+          },
+          { once: true },
+        );
+      }
+    },
+    { once: true },
+  );
+}
+
+function getActiveTiles(): NodeListOf<HTMLElement> {
   return guessGrid.querySelectorAll('[data-state="active"]');
 }
 
@@ -109,4 +168,48 @@ function showAlert(message: string, duration = 1000) {
     alert.classList.add('hide');
     alert.addEventListener('transitionend', () => alert.remove());
   }, duration);
+}
+
+function shakeTiles(tiles: Element[]) {
+  tiles.forEach((tiles) => {
+    tiles.classList.add('shake');
+    tiles.addEventListener(
+      'animationend',
+      () => {
+        tiles.classList.remove('shake');
+      },
+      { once: true },
+    );
+  });
+}
+
+function checkWinLose(guess: string, tiles: Element[]) {
+  if (guess === targetWord) {
+    showAlert('You win!', 5000);
+    danceTiles(tiles);
+    stopInteraction();
+    return;
+  }
+
+  const remainingTiles = guessGrid.querySelectorAll(':not([data-letter])');
+
+  if (remainingTiles.length === 0) {
+    showAlert(targetWord.toUpperCase(), null);
+    stopInteraction();
+  }
+}
+
+function danceTiles(tiles: Element[]) {
+  tiles.forEach((tiles, index) => {
+    setTimeout(() => {
+      tiles.classList.add('dance');
+      tiles.addEventListener(
+        'animationend',
+        () => {
+          tiles.classList.remove('dance');
+        },
+        { once: true },
+      );
+    }, (index * DANCE_ANIMATION_DURATION) / 5);
+  });
 }
